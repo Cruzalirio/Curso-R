8573b9f6fade65e96bf8d4b4dba1da00be2a747a
Cruzalirio <60365581+Cruzalirio@users.noreply.github.com>
2021-10-07 09:19:18 -0500

    Add files via upload

---
+++ Taller1.R
@@ -0,0 +1,34 @@
+
+
+##### Taller 1
+#### Debe cargar un código R que solucione las siguientes problematicas, con base a los datos
+#### de homicidios
+url="https://raw.githubusercontent.com/Cruzalirio/Ucentral/master/Bases/Homicidios.csv"
+
+### 1. Seleccione una muestra aleatoria de la base, de 2000 homicidios en un archivo
+### llamado hom_200
+
+### 2. Construya una variable llamada tipo de arma, con una categoría que sea 
+### arma tipica(Arma blanca, arma de fuego)
+### y no tipica (las demás)
+
+
+### 3. Construya un dataframe llamada meses que tenga por cada mes las siguientes variables:
+### Numero de homicidios
+### Promedio de edad
+### COnteo por genero
+
+### 4. construir un gráfico de barras con los departamentos, aunque si alguno tiene menos del 
+### 5% del total de datos, debemos agruparlo en otros. 
+
+### 5. Construir un gráfico de lineas, donde en el eje x este el mes del año y 
+###en el y el total de homicidios por mes 
+
+### 6. Buscar y hacer una de estas:
+###a.  Un grafico de mosaico entre Sexo y Estado civil
+###b. Un grafico de barras entre el sexo y los meses
+###c. Un boxplot de la edad por cada uno de los días de la semana
+
+
+##UN archivo .R por persona bien documentado, hasta el sabado a las 23:59:59.
+## Debe poderse ejecutar sin errores
